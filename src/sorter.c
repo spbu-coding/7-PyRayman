d2310bd7930e0f81eadac2e821f6f5495a1f00ca
PyRayman <rizvan.mallaev@mail.ru>
2020-12-23 23:01:13 +0300

    sorter v1.5 sorter.c + tools.c

--- src/sorter.c
+++ src/sorter.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#include "tools.h"
+//-------------------------------Tools------------------------------
 
 #define MAX_INPUT_STRING_SIZE 1000
 
@@ -15,6 +15,73 @@ void free_memory(char** array, int count){
     }
     free(array);
 }
+void add_new_line_symbol(char* string)
+{
+    int count_of_sybmol = 0;
+    while( string[count_of_sybmol] != '\n' && string[count_of_sybmol] != '\0' && count_of_sybmol < MAX_SIZE_OF_LINE){
+        count_of_sybmol++;
+    }
+    string[count_of_sybmol] = '\n';
+}
+
+void swap(char **xp, char **yp) 
+{ 
+    char *temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+
+int des_cmp(const void *first_string, const void  *second_string)
+{
+    const char  **temp1 = (const char **)first_string;
+    const char  **temp2 = (const char **)second_string;
+
+    return -strcmp(*temp1,*temp2); 
+}
+
+int check_punctuation_mark(char symbol)
+{
+    if( ( symbol == '.' ) || ( symbol == ',' ) || ( symbol == ';' ) || ( symbol == ':' )|| ( symbol == '!' ) || ( symbol == '?' ))
+    {
+        return 1;
+    }
+    else
+        return 0;
+}
+
+void rewrite( char *string , int start )
+{
+    if( start  > MAX_SIZE_OF_LINE )
+        return;
+    while( string[start] != '\n' && string[start] != '\0' )
+    {
+        string[start] = string[start + 1];
+        start ++;
+    }
+    string[start] = '\0';
+    string[start + 1] = '\0';
+
+}
+
+void deleting_symbols( char** strings, int* count_of_strings )
+{
+    for( int i = 0 ; i < *count_of_strings ; i++ )
+    {
+        int number_of_char = 0;
+        while( (strings[i][ number_of_char ] != '\n') && (strings[i][ number_of_char ] != '\0') )
+        {
+            if( check_punctuation_mark( strings[i][number_of_char] ) )
+            {
+                rewrite( strings[i] , number_of_char );
+            }
+            else
+            {
+                number_of_char++;
+            }
+        }
+    }
+}
+//-----------------------------------Main--------------------------------------------
 
 
 int main(int argc, char** argv){
--- src/tools.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int des_cmp(const void* first_string, const void* second_string);
-
-void add_new_line_symbol(char* string);
-
-void deleting_symbols(char** strings, int *count_of_strings);
